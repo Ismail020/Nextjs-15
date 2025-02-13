c5cf93a452c6e864559f56d318a98de6b39c5bdb
Ismail Çetin <isocetin1907@gmail.com>
2025-02-13 16:33:18 +0100

    Understanding Partial Pre-rendering

---
+++ app/(root)/startup/[id]/page.tsx
@@ -0,0 +1,23 @@
+import { STARTUP_BY_ID_QUERY } from "@/lib/queries";
+import { client } from "@/sanity/lib/client";
+import { notFound } from "next/navigation";
+
+export const experimental_ppr = true;
+
+export default async function page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  if (!post) return notFound();
+
+  return (
+    <>
+      <h1 className="text-3xl">{post.title}</h1>
+    </>
+  );
+}
